826de17d4612ab6f32bdd79617726cb20447851a
MaxFine <gibboel5@gmail.com>
2022-12-24 02:18:20 +0000

    Blog Dj_Girls

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.urls import reverse
 
 
+
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -19,5 +20,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
+
+
         
         
--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 
 urlpatterns = [
+    
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/edit/<int:pk>/', views.post_edit, name='post_edit'),
+    path('delete/<int:pk>/', views.PostDeleteView.as_view(), name='post_delete'),
+
 ]
+
